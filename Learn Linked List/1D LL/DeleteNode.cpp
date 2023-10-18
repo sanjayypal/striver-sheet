b706283bc3365269e496675673afeae0e3697563
sanjayypal <sanjayypal113@gmail.com>
2023-10-18 12:48:20 +0530

    furthur 1d problems

---
+++ Learn Linked List/1D LL/DeleteNode.cpp
@@ -0,0 +1,19 @@
+/**
+ * Definition for singly-linked list.
+ * class Node {
+ * public:
+ *     int data;
+ *     Node *next;
+ *     Node() : data(0), next(nullptr) {}
+ *     Node(int x) : data(x), next(nullptr) {}
+ *     Node(int x, Node *next) : data(x), next(next) {}
+ * };
+ */
+
+Node *deleteLast(Node *list){
+    Node* temp=list;
+    while(temp->next->next!=NULL)
+    temp=temp->next;
+    temp->next=NULL;
+    return list;
+}
