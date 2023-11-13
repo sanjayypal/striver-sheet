76dc49f710d9bf1153d358f5cda3497e39b763e5
Sanjay Pal <97802783+sanjayypal@users.noreply.github.com>
2023-11-13 23:58:44 +0530

    Add files via upload

---
+++ Stack & Queue/Learning/validParenthesis.cpp
@@ -0,0 +1,30 @@
+#include<stack>
+bool isValidParenthesis(string s)
+{
+  stack<char> st;
+  for(int i=0; i<s.length(); i++)
+  {
+      char ch = s[i];
+      if(ch == '(' || ch == '{' || ch == '[')
+      st.push(ch);
+      else
+      {
+      if (!st.empty()) {
+        char topch = st.top();
+        if (ch == ')' && topch == '(')
+          st.pop();
+        else if (ch == '}' && topch == '{')
+          st.pop();
+        else if (ch == ']' && topch == '[')
+          st.pop();
+        else
+          return false;
+      } else
+        return false; 
+      }
+  }
+  if(st.empty())
+  return true;
+  else
+  return false;
+}
